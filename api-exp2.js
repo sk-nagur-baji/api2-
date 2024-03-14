277674259a0377829e8570439409688af487262d
sk-nagur-baji <109502192+sk-nagur-baji@users.noreply.github.com>
2024-03-15 00:38:42 +0530

    Add files via upload

---
+++ api-exp2.js
@@ -0,0 +1,30 @@
+const users = [
+    {
+    id: 1,
+    name: 'V Bhavani Shankar',
+    email: 'vbs@gmail.com',
+    type: 'admin'
+    },
+    
+    {
+    id: 2,
+    name: 'Shaik Fasi',
+    email: 'fasi@gmail.com',
+    status: 'guest'
+    },
+    
+    {
+    id: 3,
+    name: 'Nagur Baji',
+    email: 'baji@gmail.com',
+    status: 'admin'
+    },
+
+    {
+    id: 4,
+    name: 'Puski',
+    email: 'puski@gmail.com',
+    status: 'brainless'
+    }
+];
+    module.exports = users
